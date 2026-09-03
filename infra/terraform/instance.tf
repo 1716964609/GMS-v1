af9674ff305142c20e53cdf6d5b7c8c129c1610e
白石流司・黒川龍之介 <machome@MacBook-Air.local>
2026-09-03 13:26:25 +0900

    Import GMS Spot EC2 instance into Terraform

---
+++ infra/terraform/instance.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "gms" {
+  ami           = "ami-02bf4b2a72125c870"
+  instance_type = "t3a.micro"
+  key_name      = "spring"
+
+  availability_zone = "ap-northeast-1a"
+
+  ebs_optimized = true
+  monitoring    = false
+
+  primary_network_interface {
+    network_interface_id = aws_network_interface.gms.id
+  }
+
+  instance_market_options {
+    market_type = "spot"
+
+    spot_options {
+      spot_instance_type             = "persistent"
+      instance_interruption_behavior = "stop"
+      max_price                      = "0.012200"
+    }
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "required"
+    instance_metadata_tags      = "disabled"
+  }
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 8
+    volume_type           = "gp3"
+    iops                  = 3000
+    throughput            = 125
+    encrypted             = false
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
